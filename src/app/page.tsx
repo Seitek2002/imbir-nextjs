1b547391532ed4d04e953800b86380aa5d23c624
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-29 23:18:09 +0600

    header add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { Header } from '@/widgets';
 
 export default function Home() {
   return (
     <div>
+      <Header />
       Если ты запустил проект Буран, то тебе надо зайти на страницу{' '}
       <Link href='/components' className='text-[blue] underline'>
         /components
---
+++ src/widgets/header/ui.tsx
@@ -0,0 +1,9 @@
+import Logo from '@/shared/assets/icons/header/logo.svg';
+
+export const Header = () => {
+  return (
+    <header>
+      <Logo />
+    </header>
+  );
+};
