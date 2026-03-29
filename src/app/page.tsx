8b9aa37f7dad9e45730c48e99f1d4022233297e7
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-29 19:06:56 +0600

    hint add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      Если ты запустил проект Буран, то тебе надо зайти на страницу{' '}
+      <Link href='/components' className='text-[blue] underline'>
+        /components
+      </Link>
+    </div>
+  );
 }
