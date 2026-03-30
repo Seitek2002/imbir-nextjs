2f359e0de664b9d0b9fdd2ddf4893105f8de5945
laby <laby@academy.kg>
2026-03-30 22:00:10 +0600

    feat: add project assets including images and icons with central export modules

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,27 +1,45 @@
+import ArrowLeftIcon from "./button/arrow-left.svg";
+import ArrowRightIcon from "./button/arrow-right.svg";
 import ArrowIcon from "./button/arrow.svg";
+import HeartIcon from "./cards/heart.svg";
+import PlayIcon from "./cards/play.svg";
 import DropdownArrowIcon from "./dropdown/arrow.svg";
 import DropdownCheckIcon from "./dropdown/check.svg";
+import ChevronDownIcon from "./dropdown/chevron-down.svg";
 import DropdownRemoveIcon from "./dropdown/remove.svg";
+import ChatIcon from "./header/chat.svg";
 import FilterSample from "./header/filter-sample.svg";
 import GeoBtnArrowIcon from "./header/geo-btn-arrow.svg";
 import GeoIcon from "./header/geo.svg";
 import Logo from "./header/logo.svg";
+import RemoveIcon from "./header/remove.svg";
 import SearchSample from "./header/search-sample.svg";
+import SearchIcon from "./header/search.svg";
+import UserIcon from "./header/user.svg";
 import EmailIcon from "./input/email.svg";
 import PersonIcon from "./input/person.svg";
 import WarningIcon from "./input/warning.svg";
 
 export {
   ArrowIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
   PersonIcon,
   EmailIcon,
   WarningIcon,
   DropdownArrowIcon,
   DropdownRemoveIcon,
   DropdownCheckIcon,
+  ChevronDownIcon,
   GeoIcon,
   GeoBtnArrowIcon,
   Logo,
+  SearchIcon,
+  ChatIcon,
+  UserIcon,
+  RemoveIcon,
+  HeartIcon,
+  PlayIcon,
   SearchSample,
   FilterSample,
 };
--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,4 +1,73 @@
+import BannerImage1 from "./banners/banner-1.png";
+import BannerImage2 from "./banners/banner-2.png";
+import BannerImage3 from "./banners/banner-3.png";
+import BlogImage1 from "./blog/blog-1.png";
+import BlogImage2 from "./blog/blog-2.png";
+import BlogImage3 from "./blog/blog-3.png";
+import BlogBanner from "./blog/blog-banner.png";
+import ClinicImage1 from "./clinics/clinic-1.png";
+import ClinicImage2 from "./clinics/clinic-2.png";
+import ClinicImage3 from "./clinics/clinic-3.png";
+import ClinicImage4 from "./clinics/clinic-4.png";
+import DoctorImage1 from "./doctors/doctor-1.png";
+import DoctorImage2 from "./doctors/doctor-2.png";
+import DoctorImage3 from "./doctors/doctor-3.png";
+import DoctorImage4 from "./doctors/doctor-4.png";
+import DoctorImage5 from "./doctors/doctor-5.png";
+import DoctorImage6 from "./doctors/doctor-6.png";
+import DoctorImage7 from "./doctors/doctor-7.png";
+import DoctorImage8 from "./doctors/doctor-8.png";
 import HeroAvatar from "./hero/avatar.png";
 import HeroBg from "./hero/bg.png";
+import SpecialtyCardiology from "./specialties/cardiology.png";
+import SpecialtyDentistry from "./specialties/dentistry.png";
+import SpecialtyGastroenterology from "./specialties/gastroenterology.png";
+import SpecialtyGinecology from "./specialties/ginecology.png";
+import SpecialtyLor from "./specialties/lor.png";
+import SpecialtyNevrology from "./specialties/nevrology.png";
+import SpecialtyOphthalmology from "./specialties/ophthalmology.png";
+import SpecialtyPulmonology from "./specialties/pulmonology.png";
+import VideoThumbnail4 from "./videos/video-1.jpg";
+import VideoThumbnail1 from "./videos/video-1.png";
+import VideoThumbnail5 from "./videos/video-2.jpg";
+import VideoThumbnail2 from "./videos/video-2.png";
+import VideoThumbnail6 from "./videos/video-3.jpg";
+import VideoThumbnail3 from "./videos/video-3.png";
 
-export { HeroBg, HeroAvatar };
+export {
+  HeroBg,
+  HeroAvatar,
+  DoctorImage1,
+  DoctorImage2,
+  DoctorImage3,
+  DoctorImage4,
+  DoctorImage5,
+  DoctorImage6,
+  DoctorImage7,
+  DoctorImage8,
+  ClinicImage1,
+  ClinicImage2,
+  ClinicImage3,
+  ClinicImage4,
+  SpecialtyLor,
+  SpecialtyNevrology,
+  SpecialtyGinecology,
+  SpecialtyCardiology,
+  SpecialtyPulmonology,
+  SpecialtyOphthalmology,
+  SpecialtyGastroenterology,
+  SpecialtyDentistry,
+  BannerImage1,
+  BannerImage2,
+  BannerImage3,
+  VideoThumbnail1,
+  VideoThumbnail2,
+  VideoThumbnail3,
+  VideoThumbnail4,
+  VideoThumbnail5,
+  VideoThumbnail6,
+  BlogBanner,
+  BlogImage1,
+  BlogImage2,
+  BlogImage3,
+};
