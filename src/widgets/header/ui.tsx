f2d62e2b44b39e30f7401d7ade534691cdd2c1d4
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-30 14:50:20 +0600

    adaptive

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,30 +1,41 @@
 import Link from "next/link";
 
-import { Button } from "@/shared";
+import { Button, IconBtn } from "@/shared";
 
 import {
-  FilterSample,
+  ChatIcon,
   GeoBtnArrowIcon,
   GeoIcon,
   Logo,
+  ProfileIcon,
+  SearchIcon,
   SearchSample,
 } from "@/shared/assets";
 
 export const Header = () => {
   return (
-    <header className="px-4 pb-4 bg-white rounded-br-2xl rounded-bl-2xl">
-      <div className="flex items-center justify-between">
+    <header className="px-4 pb-4 bg-white md:border md:border-[#E3E4E5] rounded-br-2xl md:rounded-full rounded-bl-2xl md:mt-6 md:py-3 w-full max-w-360 mx-auto">
+      <div className="flex items-center justify-between w-full">
         <Logo />
 
-        <nav className="hidden md:flex text-[#191A1B] text-base gap-6 items-center">
+        <nav className="hidden md:flex text-[#191A1B] text-xs lg:text-base gap-6 items-center">
           <Link href={"#"}>Клиники</Link>
           <Link href={"#"}>Специалисты</Link>
           <Link href={"#"}>Услуги</Link>
           <Link href={"#"}>Блог</Link>
         </nav>
 
-        <div className="hidden md:flex">
-          <Button size="md">Записаться на приём</Button>
+        <div className="hidden md:flex gap-3">
+          <Button size="xs">Записаться на приём</Button>
+          <IconBtn variant="outline" size="sm">
+            <SearchIcon className="size-5" />
+          </IconBtn>
+          <IconBtn variant="outline" size="sm">
+            <ChatIcon className="size-5" />
+          </IconBtn>
+          <IconBtn variant="outline" size="sm">
+            <ProfileIcon className="size-5" />
+          </IconBtn>
         </div>
 
         <Button
@@ -37,9 +48,8 @@ export const Header = () => {
           Бишкек
         </Button>
       </div>
-      <div className="flex items-center justify-between mt-4">
+      <div className="flex items-center justify-center mt-4 md:hidden">
         <SearchSample className="active:scale-95 transition-transform" />
-        <FilterSample className="active:scale-95 transition-transform" />
       </div>
     </header>
   );
