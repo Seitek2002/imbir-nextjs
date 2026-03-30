0ca22ce2339868f85823c7c670f4774061136edc
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-30 23:25:35 +0600

    header adaptived

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -16,24 +16,36 @@ export const Header: FC<Props> = ({ children, title, backTo, searchable }) => {
   return (
     <header
       className={cn(
-        "px-4 pt-1 pb-4 bg-white md:border md:border-[#E3E4E5] md:mt-6 md:py-3 w-full max-w-360 mx-auto",
-        title || backTo ? "" : " rounded-bl-2xl rounded-br-2xl md:rounded-full",
+        "px-4 pt-1 pb-4 bg-white md:border md:border-[#E3E4E5] md:mt-6 md:py-3 w-full max-w-360 md:max-w-300 mx-auto",
+        !(title || backTo)
+          ? "rounded-bl-2xl rounded-br-2xl md:rounded-full"
+          : "md:rounded-full",
       )}
     >
-      {title || backTo ? (
-        <div>
-          <div className="grid grid-cols-3 items-center">
-            <div>
-              <BackButton backTo={backTo} />
+      <div className="hidden md:block">
+        <DefaultContent searchable={false} />
+      </div>
+
+      <div className="md:hidden">
+        {title || backTo ? (
+          <div>
+            <div className="grid grid-cols-3 items-center min-h-10">
+              <div>
+                <BackButton backTo={backTo} />
+              </div>
+              <h2 className="text-center font-medium text-base truncate px-2">
+                {title}
+              </h2>
+              <div className="flex justify-end">
+                {/* Место для иконки справа, если понадобится */}
+              </div>
             </div>
-            <h2 className="text-center font-medium text-base">{title}</h2>
-            <div></div>
+            {children && <div className="mt-4">{children}</div>}
           </div>
-          <div>{children}</div>
-        </div>
-      ) : (
-        (children ?? <DefaultContent searchable={searchable} />)
-      )}
+        ) : (
+          <DefaultContent searchable={searchable} />
+        )}
+      </div>
     </header>
   );
 };
