d8225b7604bb72538fbca0a8315a18c4fa5c9dac
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-31 10:52:30 +0600

    clear home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Link from "next/link";
-
 import { HomePage } from "@/views";
 
 export default function Home() {
-  return (
-    <>
-      Если ты запустил проект Буран, то тебе надо зайти на страницу{" "}
-      <Link href="/components" className="text-[blue] underline">
-        /components
-      </Link>
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 }
