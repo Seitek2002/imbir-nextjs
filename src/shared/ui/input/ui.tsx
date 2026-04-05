c2f7750ef41dc5b2294aa00304ded72228fbe644
Seitek2002 <seitekbegaliev@gmail.com>
2026-04-05 15:07:34 +0600

    input changed

--- src/shared/ui/input/ui.tsx
+++ src/shared/ui/input/ui.tsx
@@ -11,6 +11,7 @@ type Props = {
   label?: string;
   className?: string;
   labelClassName?: string;
+  iconClassName?: string;
   IconLeft?: IconType;
   IconRight?: IconType;
   size?: Sizes;
@@ -23,12 +24,14 @@ export const Input: FC<Props> = ({
   label,
   className,
   labelClassName,
+  iconClassName,
   error,
   IconLeft,
   IconRight,
   size = "small" as Sizes,
   hint,
   onIconRightClick,
+  type = "text",
   ...props
 }) => {
   const sizes: Record<Sizes, string> = {
@@ -37,23 +40,37 @@ export const Input: FC<Props> = ({
   };
 
   const baseStyle =
-    "text-base leading-0 border border-[#E3E4E5] text-[#191A1B] rounded-lg py-2.25 px-3 outline-none focus:shadow-[0_0_1px_3px_rgba(245,101,62,0.3)] disabled:bg-[#F2F3F5] " +
+    "text-base leading-0 border border-[#E3E4E5] text-[#191A1B] rounded-lg py-2.25 px-3 outline-none focus:shadow-[0_0_1px_3px_rgba(245,101,62,0.3)] disabled:bg-[#F2F3F5] w-full " +
     sizes[size];
 
   const isIconLeft = IconLeft && "pl-10";
 
   const isError =
-    error && "shadow-[0_0_1px_3px_rgba(223,28,65,0.3)] border-[#EC778D]";
+    error &&
+    "shadow-[0_0_1px_3px_rgba(223,28,65,0.3)] border-[#EC778D] focus:shadow-[0_0_1px_3px_rgba(223,28,65,0.3)]";
 
   return (
-    <label htmlFor={props.id} className={cn("block", labelClassName)}>
-      <span className="text-[#0D0D12] text-sm font-medium">{label}</span>
+    <label
+      htmlFor={props.id}
+      className={cn("flex flex-col gap-1.5", labelClassName)}
+    >
+      {label && (
+        <span className="text-[#0D0D12] text-sm font-medium">{label}</span>
+      )}
+
       <div className="relative w-full">
-        {IconLeft && <IconLeft className="absolute left-3 top-2.25 size-5" />}
+        {IconLeft && (
+          <IconLeft
+            className={cn(
+              "absolute left-3 top-1/2 -translate-y-1/2 size-5 text-[#191A1B]",
+              iconClassName,
+            )}
+          />
+        )}
 
         <input
           id={props.id}
-          type={props.type && "text"}
+          type={type}
           placeholder={props.placeholder}
           className={cn(baseStyle, isIconLeft, isError, className)}
           disabled={props.disabled}
@@ -62,20 +79,23 @@ export const Input: FC<Props> = ({
 
         {IconRight && (
           <IconRight
-            className="absolute right-3 top-2.5 size-5"
+            className={cn(
+              "absolute right-3 top-1/2 -translate-y-1/2 size-5 text-[#191A1B] cursor-pointer",
+              iconClassName,
+            )}
             onClick={onIconRightClick}
           />
         )}
       </div>
 
       {error && (
-        <span className="flex items-center gap-1 text-sm mt-1.5">
-          <WarningIcon className="size-4" />
+        <span className="flex items-center gap-1 text-sm mt-0.5">
+          <WarningIcon className="size-4 shrink-0" />
           <span className="text-[#DF1C41]">{error}</span>
         </span>
       )}
 
-      {hint && <span className="text-sm text-[#838A8D] mt-1.5">{hint}</span>}
+      {hint && <span className="text-sm text-[#838A8D] mt-0.5">{hint}</span>}
     </label>
   );
 };
