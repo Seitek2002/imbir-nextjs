37ca28250ff98e5aa35460788e80a11fef6f496a
Seitek2002 <seitekbegaliev@gmail.com>
2026-04-06 19:16:50 +0600

    forgot password add

--- src/views/index.ts
+++ src/views/index.ts
@@ -7,3 +7,4 @@ export { ClinicsPage } from "./clinics";
 export { ServicesPage } from "./services";
 export { RegisterPage } from "./register";
 export { LoginPage } from "./login";
+export { ForgotPasswordPage } from "./forgot-password";
