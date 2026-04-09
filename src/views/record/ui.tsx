ea605be50ee78676e1411cda1d4460410ed7c0af
laby <laby@academy.kg>
2026-04-09 13:47:04 +0600

    fix

--- src/views/record/ui.tsx
+++ src/views/record/ui.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { FC, useMemo, useState } from "react";
 
 import Image, { StaticImageData } from "next/image";
 import { useRouter } from "next/navigation";
